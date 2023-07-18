8e40b6215381ea46257c1e7cb1f65c43b7bae151
Leonhard Horstmeyer <leo.h@hotmail.de>
2023-07-18 13:22:50 +0300

    added a dockerdown file

--- tests/integration/dockerdown.py
+++ tests/integration/dockerdown.py
@@ -5,14 +5,19 @@ import os
 import subprocess
 
 def dockerdown():
-    os.chdir('../..')
-    os.chdir('manualTest')
+    # the the directory of this file, then go two directories up and then go to the manualTest directory and docker-compose down. Use os.path.join
+    os.chdir(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'manualTest'))
     subprocess.run(['docker-compose', 'down'])
 
 # remove the folder manualTest  
 def removeManualTest():
-    os.chdir('..')
-    os.rmdir('manualTest')
+    os.chdir(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+    # print(os.getcwd())
+    # remove recursively the manualTest directory
+    os.system('sudo rm -rf manualTest')
+    # # the directory of this file, then go two directories up and then remove manualTest directory
+    # os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    # os.rmdir('manualTest')
 
 if __name__ == '__main__':
     dockerdown()
