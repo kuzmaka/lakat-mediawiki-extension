60e0020ef88dddca2b0dd414c45b547086b005f8
Leonhard Horstmeyer <leo.h@hotmail.de>
2023-07-20 17:27:01 +0300

    can push new modifications of Lakat to the manual test

--- tests/integration/pushLakatExtension.py
+++ tests/integration/pushLakatExtension.py
@@ -11,3 +11,6 @@ def CopyLakatToManualTest():
     os.chdir(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
     # copy the lakat folder into the manualTest / extensions folder using os.path.join  
     os.system('cp -r Lakat manualTest/extensions')
+
+if __name__ == '__main__':
+    CopyLakatToManualTest()
