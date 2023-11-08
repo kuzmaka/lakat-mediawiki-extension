5d5dc036ad722e2b486c812d747588f3063f3527
Kuzma Kudim <kuzmaka@gmail.com>
2023-11-08 20:36:58 +0200

    fix namespace for older mediawiki

--- Lakat/src/SpecialCreateBranch.php
+++ Lakat/src/SpecialCreateBranch.php
@@ -2,51 +2,52 @@
 
 namespace MediaWiki\Extension\Lakat;
 
-use MediaWiki\SpecialPage\FormSpecialPage;
+// use MediaWiki\SpecialPage\FormSpecialPage;   // this is for newer mediawiki
+use FormSpecialPage;
 
 class SpecialCreateBranch extends FormSpecialPage
 {
-    public function __construct()
-    {
-        parent::__construct('CreateBranch');
-    }
+	public function __construct()
+	{
+		parent::__construct('CreateBranch');
+	}
 
-    protected function getDisplayFormat() {
-        return 'ooui';
-    }
+	protected function getDisplayFormat() {
+		return 'ooui';
+	}
 
-    protected function getFormFields()
-    {
-        $formDescriptor = [
-            'BranchName' => [
-                'type' => 'text',
-                'label-message' => 'createbranch-name',
-            ],
-            'Token' => [
-                'type' => 'text',
-                'label-message' => 'createbranch-token',
-            ],
-            'BranchType' => [
-                'type' => 'radio',
-                'flatlist' => 1,
-                'options-messages' => [
-                    'createbranch-type-proper' => 'proper',
-                    'createbranch-type-twig' => 'twig'
-                ],
-                'default' => 'proper',
-            ],
-            'AllowConflicts' => [
-                'type' => 'check',
-                'label-message' => 'createbranch-allow-conflicts',
-                'value' => 0,
-            ],
-        ];
+	protected function getFormFields()
+	{
+		$formDescriptor = [
+			'BranchName' => [
+				'type' => 'text',
+				'label-message' => 'createbranch-name',
+			],
+			'Token' => [
+				'type' => 'text',
+				'label-message' => 'createbranch-token',
+			],
+			'BranchType' => [
+				'type' => 'radio',
+				'flatlist' => 1,
+				'options-messages' => [
+					'createbranch-type-proper' => 'proper',
+					'createbranch-type-twig' => 'twig'
+				],
+				'default' => 'proper',
+			],
+			'AllowConflicts' => [
+				'type' => 'check',
+				'label-message' => 'createbranch-allow-conflicts',
+				'value' => 0,
+			],
+		];
 
-        return $formDescriptor;
-    }
+		return $formDescriptor;
+	}
 
-    public function onSubmit(array $data)
-    {
-        return true;
-    }
+	public function onSubmit(array $data)
+	{
+		return true;
+	}
 }
